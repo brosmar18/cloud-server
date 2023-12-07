ef76039461193ab18a148aa5f6b7d0caf194f78e
brosmar18 <bryangonzalez1896@icloud.com>
2023-12-07 17:37:58 -0600

    hello route added

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.get('/', (req, res, next) => {
     res.status(200).send('Hello World!');
 });
 
+app.get('/hello', (req, res, next) => {
+    res.status(200).send('Hello Everyone! The cloud server is launched and should be working!');
+});
 
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
